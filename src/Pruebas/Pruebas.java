1ddebf4472eb1826bb33fa48fda88ab8de1e6484
pascu <pascu@Alienware>
2020-09-17 14:30:03 -0300

    Agrega Despedida

--- src/Pruebas/Pruebas.java
+++ src/Pruebas/Pruebas.java
@@ -1,9 +1,15 @@
 package Pruebas;
 
+import java.util.Scanner;
+
 public class Pruebas {
 
 	public static void main(String[] argumentos) {
 		System.out.println("Hola Mundo");
+		Scanner NombreUsuario = new Scanner(System.in);
+		String Nombre = NombreUsuario.nextLine();
+		
+		System.out.println("Chau " + Nombre);
 	}
 	
 
